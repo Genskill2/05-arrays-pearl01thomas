182c41904f26c5435ed82232498dbd4776d152cb
pearl01thomas <84620247+pearl01thomas@users.noreply.github.com>
2021-06-18 22:04:57 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 
-int mode(int [], int);
+int mode(int arr[], int s);
 
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
@@ -12,3 +12,21 @@ int main(void) {
   assert (mode(y, 1) == 5);
   printf("Mode: passed\n");
   }
+int mode(int arr[], int s)
+{
+   int x=1;
+  int y=arr[0];
+   for(int i=0;i<s;i++)
+   {int c=1;
+    
+    for(j=i+1;j<n;j++)
+      {  if(a[i]==a[j])
+          {count++;}
+      }
+       if(c>x)
+         {x=count;
+         y=arr[i];
+         }
+    }
+  return y'
+ }    
